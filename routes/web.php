c57f01252ce994fda0d70d0905b73281a8654fd4
cola <965998020@qq.com>
2017-04-12 17:23:46 +0800

    new file

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::group(['domain' => 'm.hankele.cn'], function()
+{
+
+    Route::get('/','Wap\IndexController@index');
+
 });
